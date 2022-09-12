c98ec3b8a530caadb3e7e559b881e1acb20f6860
tepla18 <93670533+tepla18@users.noreply.github.com>
2022-09-12 18:11:49 -0500

    clase 08-09-22 vistas login, registro, error

--- src/main/java/com/example/prestamos/controllers/LoginController.java
+++ src/main/java/com/example/prestamos/controllers/LoginController.java
@@ -1,13 +1,23 @@
 package com.example.prestamos.controllers;
 
+import com.example.prestamos.entities.User;
+import com.example.prestamos.services.Response;
+import com.example.prestamos.services.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 @RequestMapping("login")
 public class LoginController {
 
+    private UserService service;
+
+    public LoginController(UserService service){
+        this.service = service;
+    }
     @GetMapping("login")
     public String login(){
         return "login/login";
@@ -16,5 +26,21 @@ public class LoginController {
     @GetMapping("registro")
     public String registro(){
         return "login/registro";
+    }
+
+    @PostMapping("postlogin")
+    public RedirectView postlogin(User data){
+        Response response = this.service.loginUser(data);
+        if (response.getCode() == 200){
+            return new RedirectView("/index");
+        }
+        else{
+            return new RedirectView("/login/error");
+        }
+    }
+
+    @GetMapping("error")
+    public String error(){
+        return "login/error";
     }
 }
